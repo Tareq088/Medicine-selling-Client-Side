aca1cc12f27e7cde644ba070750128cedc7979a1
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-07-09 00:19:07 +0600

    authprovider, toastify, sweetalert2 is installled

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+import {RouterProvider,} from "react-router";
+import { router } from './Router/Router.jsx';
+import AuthProvider from './Contexts/AuthProvider.jsx';
+import { ToastContainer } from 'react-toastify';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+
+    <AuthProvider>
+        <RouterProvider router={router} />
+        <ToastContainer/>
+    </AuthProvider>
+
     
   </StrictMode>,
 )
