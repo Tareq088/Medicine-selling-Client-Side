90d39b52c70bc739e42c9cbceb16f2eb7ba650b4
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-07-11 07:48:47 +0600

    Manage user is done

--- src/Pages/Dashboard/ManageUsers/UsersTable.jsx
+++ src/Pages/Dashboard/ManageUsers/UsersTable.jsx
@@ -1,15 +1,31 @@
 import { useMutation } from "@tanstack/react-query";
 import React, { useState } from "react";
+import useAxiosSecure from './../../../Hooks/useAxiosSecure';
+import { toast } from "react-toastify";
 
 const UsersTable = ({ user,index,refetch}) => {
+              // user mane sokol user
 //   console.log(user);
                     //  user jodi na thake taile jeno error na ashe tai ||{} dilam
   const {name, email, role, createdAt,_id} = user || {};
+  const axiosSecure = useAxiosSecure();
   const[userRole, setUserRole] = useState("");
-//   const {} = useMutation({
-//     mutationFn
-//   })
-  console.log(userRole);
+  // const mutationRole = useMutation({
+  //   mutationFn: async({role}) =>{
+  //     const res = await axiosSecure.patch("/users/role/${user.email}")
+  //   }
+  // })
+  // console.log(userRole);
+  const handleRoleChange = async(e,userId,userEmail, userRole, userName) =>{
+    console.log(userId,userEmail, userRole);
+    const res =  await axiosSecure.patch(`/users/role/${userEmail}`,{role:e.target.value})
+    console.log(res.data);
+    if(res.data.modifiedCount){
+      toast.success(`${userName} is changed to ${userRole} role`)
+      refetch();
+    }
+    
+  }
 
   return (
       <tr >
@@ -21,8 +37,11 @@ const UsersTable = ({ user,index,refetch}) => {
         <td className="text-base">
             <select name="role" 
                 className="select"
-                value={userRole} 
-                onChange={(e)=>{setUserRole(e.target.value)}}>
+                onChange={(e)=>{handleRoleChange(e,_id, email, role, name)}}
+                value={role}
+                // value={userRole} 
+                // onChange={(e)=>{setUserRole(e.target.value)}}
+                >
                 <option value="admin">Admin</option>
                 <option value="seller">Seller</option>
                 <option value="user">User</option>
